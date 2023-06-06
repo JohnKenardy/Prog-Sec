5c6f432553c79d66530c35d3ed8ab29c20a6ea53
JohnKenardy <44053561+JohnKenardy@users.noreply.github.com>
2023-06-06 19:28:21 +1000

    edit item

--- form_handlers/edit/edit_customer_submit.php
+++ form_handlers/edit/edit_customer_submit.php
@@ -5,32 +5,30 @@ require_once "../../scripts/config.php";
 
 echo("Config loaded");
 
-
-$customerID = addslashes($_POST['customerID']);
-$customerName = addslashes($_POST['customerName']);
-$companyID = addslashes($_POST['companyID']);
-$role = addslashes($_POST['role']);
-$userID = addslashes($_POST['userID']);
+$itemID = addslashes($_POST['itemID']);
+$itemName = addslashes($_POST['itemName']);
+$itemRFID = addslashes($_POST['itemRFID']);
 
 //check if any variable is empty
-if(empty($customerName) || empty($companyID) || empty($role) || empty($userID)){
-    header("location: ../../edit_customer.php?e=1");
+//check if any variable is empty
+if(empty($itemID) || $itemName) || empty($itemRFID)){
+    header("location: ../../add_item.php?e=1");
     exit;
 }
 
 echo("Values loaded");
 
 $sql = "
-    UPDATE Customers 
-    SET customerName = '$customerName', companyID = '$companyID', role = '$role', userID = '$userID', customerUserID = '$UserID', customerDateTime = NOW()
-    WHERE customerID = '$customerID'
+    UPDATE Items 
+    SET itemName = '$itemName', itemRFID = '$itemRFID', createUserID = '$UserID', createDateTime = NOW()
+    WHERE itemID = '$itemID'
     ";
 
 echo("SQL defined");
 
 if ($link->query($sql) === TRUE) {
     echo("SQL Success");
-	header("location: ../../view_customers.php"); //update to view customers
+	header("location: ../../view_items.php"); //update to view all items
 }else {
     echo("Error");
     echo "Error: " . $link->error;
