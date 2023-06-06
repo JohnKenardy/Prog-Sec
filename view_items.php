3d5caade544003cf45b1f8fb85eaba9f82e00457
JohnKenardy <44053561+JohnKenardy@users.noreply.github.com>
2023-06-06 19:06:09 +1000

    view items

--- view_items.php
+++ view_items.php
@@ -9,7 +9,7 @@ include("scripts/sessioncheck.php");
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Viden LIMS - View Customers</title>
+        <title>RFID Tool - View Items</title>
         <?php
             include("layout/style.php");
         ?>
@@ -23,40 +23,40 @@ include("scripts/sessioncheck.php");
         <!-- !PAGE CONTENT! --> 
         <div class="w3-main" style="margin-left:250px;margin-top:43px;">
             <div class="w3-panel">
-            <h1>View Customers</h1>
-            <a href="add_customer.php" class="w3-button w3-padding w3-blue">Create New Customer</a>            
+            <h1>View Items</h1>
+            <a href="add_item.php" class="w3-button w3-padding w3-blue">Create New Item</a>            
             
-            <form method="GET" action="view_customers.php" <?php if (isset($_GET['customer'])) echo 'style="display: none;"'; ?>>
+            <form method="GET" action="view_items.php" <?php if (isset($_GET['item'])) echo 'style="display: none;"'; ?>>
                 <input type="text" name="search" placeholder="Search by name" value="<?php echo isset($_GET['search']) ? $_GET['search'] : ''; ?>">
                 <input type="submit" value="Search" class="w3-button w3-padding">
 
                 <?php if (isset($_GET['search'])): ?>
-                    <a href="view_customers.php" class="w3-button w3-padding w3-blue">View All</a>
+                    <a href="view_items.php" class="w3-button w3-padding w3-blue">View All</a>
                 <?php endif; ?>
             </form>
 
             <?php
             // Check if a search query is provided
-            if (isset($_GET['search'])) {
-                $search = $_GET['search'];
+            if (isset($_GET['item'])) {
+                $search = $_GET['item'];
 
-                // Query to fetch matching customers
-                $sql = "SELECT * FROM Customers WHERE customerName LIKE '%$search%';";
+                // Query to fetch matching item
+                $sql = "SELECT * FROM Items WHERE itemName LIKE '%$search%';";
 
                 require_once "scripts/config.php";
 
                 $result = $link->query($sql);
 
                 if ($result->num_rows > 0) {
-                    // Display matching customers
+                    // Display matching items
                     while ($row = $result->fetch_assoc()) {
                         echo "<div class='w3-card-4 w3-margin w3-white'>";
                         echo "<div class='w3-container'>";
-                        echo "<h3><b>" . $row["customerName"] . "</b></h3>";
+                        echo "<h3><b>" . $row["itemName"] . "</b></h3>";
 
                         echo "<div class='w3-row'>";
                         echo "<div class='w3-col m8 s12'>";
-                        echo "<p><a href='view_customers.php?customer=" . $row["customerID"] . "'><button class='w3-button w3-padding-large w3-white w3-border'><b>View Customer Details</b></button></a></p>";
+                        echo "<p><a href='view_items.php?item=" . $row["itemID"] . "'><button class='w3-button w3-padding-large w3-white w3-border'><b>View Item Details</b></button></a></p>";
                         echo "</div>";
                         echo "<div class='w3-col m4 w3-hide-small'>";
                         echo "</div>";
@@ -65,17 +65,17 @@ include("scripts/sessioncheck.php");
                         echo "</div>";
                     }
                 } else {
-                    echo "<h3>No matching customers found.</h3>";
+                    echo "<h3>No matching items found.</h3>";
                 }
             } 
-            elseif(isset($_GET['customer'])){
-                echo('<a href="view_customers.php" class="w3-button w3-blue">Back to All Customers</a>');
+            elseif(isset($_GET['item'])){
+                echo('<a href="view_items.php" class="w3-button w3-blue">Back to All items</a>');
 
-                echo('<h1>Customer ID: ' .  $_GET['customer'] . '</h1>');
+                echo('<h1>item ID: ' .  $_GET['item'] . '</h1>');
 
                 $sql = "SELECT * 
-                        FROM Customers
-                        WHERE customerID = " . $_GET['customer'] . "
+                        FROM items
+                        WHERE itemID = " . $_GET['item'] . "
                         ;";
                 
                 require_once "scripts/config.php";
@@ -83,35 +83,33 @@ include("scripts/sessioncheck.php");
                 $result = $link->query($sql);
 
                 if ($result->num_rows < 0) {
-                    echo('CUSTOMER ID DOES NOT EXIST');
+                    echo('item ID DOES NOT EXIST');
                 }else{
                     while($row = $result->fetch_assoc()) {
-                        echo "<h5><b>Customer Name: </b>" . $row["customerName"] . "</h5>";
-                        echo "<h5><b>Companyid: </b>" . $row["companyID"] . "</h5>";
-                        echo "<h5><b>Role: </b>" . $row["role"] . "</h5>";
-                        echo "<h5><b>Company ID: </b>" . @$row["userID"] . "</h5>";
+                        echo "<h5><b>Item Name: </b>" . $row["itemName"] . "</h5>";
+                        echo "<h5><b>Companyid: </b>" . $row["itemRFID"] . "</h5>";
                     }
-                    echo('<a href="edit_customer.php?customerID=' . $_GET['customer'] . '" class="w3-button w3-blue">Edit</a>');
+                    echo('<a href="edit_item.php?itemID=' . $_GET['item'] . '" class="w3-button w3-blue">Edit</a>');
 
                 }
             }else {
-                // Display all customers
-                $sql = "SELECT * FROM Customers;";
+                // Display all items
+                $sql = "SELECT * FROM Items;";
 
                 require_once "scripts/config.php";
 
                 $result = $link->query($sql);
 
                 if ($result->num_rows > 0) {
-                    // Display all customers
+                    // Display all items
                     while ($row = $result->fetch_assoc()) {
                         echo "<div class='w3-card-4 w3-margin w3-white'>";
                         echo "<div class='w3-container'>";
-                        echo "<h3><b>" . $row["customerName"] . "</b></h3>";
+                        echo "<h3><b>" . $row["itemName"] . "</b></h3>";
 
                         echo "<div class='w3-row'>";
                         echo "<div class='w3-col m8 s12'>";
-                        echo "<p><a href='view_customers.php?customer=" . $row["customerID"] . "'><button class='w3-button w3-padding-large w3-white w3-border'><b>View Customer Details</b></button></a></p>";
+                        echo "<p><a href='view_items.php?item=" . $row["itemID"] . "'><button class='w3-button w3-padding-large w3-white w3-border'><b>View Item Details</b></button></a></p>";
                         echo "</div>";
                         echo "<div class='w3-col m4 w3-hide-small'>";
                         echo "</div>";
